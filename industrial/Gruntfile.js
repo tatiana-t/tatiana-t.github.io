1ab5370cc064b2fd4b51d2ad567bfe9634185a66
Tetiana Poire <grabli_77@mail.ru>
2017-06-14 15:18:44 +0300

    portfolio start

---
+++ industrial/Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function (grunt) {
+  grunt.loadNpmTasks("grunt-contrib-less");
+  grunt.loadNpmTasks("grunt-postcss");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+
+  grunt.initConfig({
+    less: {
+      style: {
+        files: {
+          "css/style.css": "less/style.less"
+        }
+      }
+    },
+    postcss: {
+      style: {
+        src: "css/*.css"
+      },
+      options: {
+        processors: [
+          require("autoprefixer")({
+            browsers:["last 2 versions"]
+          })
+        ]
+      }
+    },
+    watch: {
+      style: {
+        files: ["less/**/*.less"],
+        tasks: ["less", "postcss"]
+      }
+    }
+  });
+  grunt.registerTask("build", [
+    "less",
+    "postcss",
+    "watch"
+  ]);
+};
