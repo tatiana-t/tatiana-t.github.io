ce0bf05cd16ff625ac032aec6379c88f4c507b96
Tetiana Poire <grabli_77@mail.ru>
2017-08-02 13:20:46 +0300

    'popup done'# Please enter the commit message for your changes. Lines starting

--- portfolio.js
+++ portfolio.js
@@ -1,39 +1,35 @@
-
 window.onload = init;
 
 function init() {
-
   var btns = document.querySelectorAll('.project__button');
   for (var i = 0; i < btns.length; i++) {
-    btns[i].onclick = detailWindow;
+    btns[i].onclick = detailOpen;
   }
 
 }
 
+function detailOpen(eventObj) {
 
-var portfolioContent = document.querySelector('.portfolio__contacts');
-
-function detailWindow(eventObj) {
-  
   var btn = eventObj.target;
-  
-  console.log('btn attribute is ' + btn.getAttribute('name'));
-  
-//  var doc= document.querySelector('link[rel="import"]').import;
-  
-// задаем переменную для тега ссылки импорта, чтобы сформировать путь загружаемого файла
-  
-  var imp = document.querySelector('link[rel="import"]');
-  imp.href = 'projectDetails/' + btn.getAttribute('name') + '.html';
-  console.log(imp);
-  console.log(imp.href);
-  
-  // задаем переменную для импортируемого файла 
-  var details = document.querySelector('link[rel="import"]').import;
-  console.log(details);
-  
-  //var innerDetails = details.querySelector('.detail');
-  //console.log(innerDetails);
-  
-  //portfolioContent.appendChild.innerDetails.cloneNode(true);
+  var detailWin = btn.parentElement.parentElement.lastElementChild;
+  console.log(btn);
+  if (detailWin.classList.contains('detail--closed')) {
+    detailWin.classList.remove('detail--closed');
+    detailWin.classList.add('detail--opened');
+  };
+
+  var closeBtn = btn.parentElement.nextElementSibling.firstElementChild.nextElementSibling.firstElementChild;
+  console.log(closeBtn);
+  closeBtn.onclick = detailClose;
+
+
+  function detailClose() {
+    console.log('start close');
+
+    var detail = btn.parentElement.parentElement.lastElementChild;
+    detail.classList.add('detail--closed');
+    detail.classList.remove('detail--opened');
+    
+    console.log('end close');
+  }
 }
