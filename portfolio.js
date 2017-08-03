5f0773ef0a99a25ad97ad4ece6470a94a0047ff7
Tetiana Poire <grabli_77@mail.ru>
2017-08-03 17:08:35 +0300

    js cleaned

--- portfolio.js
+++ portfolio.js
@@ -12,24 +12,20 @@ function detailOpen(eventObj) {
 
   var btn = eventObj.target;
   var detailWin = btn.parentElement.parentElement.lastElementChild;
-  console.log(btn);
+ 
   if (detailWin.classList.contains('detail--closed')) {
     detailWin.classList.remove('detail--closed');
     detailWin.classList.add('detail--opened');
   };
 
   var closeBtn = btn.parentElement.nextElementSibling.firstElementChild.nextElementSibling.firstElementChild;
-  console.log(closeBtn);
   closeBtn.onclick = detailClose;
 
 
   function detailClose() {
-    console.log('start close');
 
     var detail = btn.parentElement.parentElement.lastElementChild;
     detail.classList.add('detail--closed');
     detail.classList.remove('detail--opened');
-    
-    console.log('end close');
   }
 }
