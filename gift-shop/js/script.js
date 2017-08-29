56020c20f90b0f54fffa978abdb551d2c9c90edd
Tetiana Poire <grabli_77@mail.ru>
2017-08-29 22:01:09 +0300

    mobile gift shop new slider

--- gift-shop/js/script.js
+++ gift-shop/js/script.js
@@ -4,17 +4,14 @@ for (i = 0; i < menu.length - 1; i++) {
 
   menu[i].addEventListener('click', function () {
     var openMenu = document.querySelector('.menu-item--open');
-    
+
     if (this.classList.contains('menu-item--open')) {
       this.classList.remove('menu-item--open');
-      
 
     } else {
-
       this.classList.add('menu-item--open');
-
     }
-if (openMenu) {
+    if (openMenu) {
       openMenu.classList.remove('menu-item--open');
     }
 
@@ -35,6 +32,7 @@ if (openMenu) {
 //if (document.body.clientWidth>800){
 //   document.write("<script>console.log('hello!');<\/script>
 
+var screenWidth = document.body.clientWidth;
 
 var wrap = document.createElement('div'); //обертка для слайдера блока promo на моб версии
 
@@ -66,7 +64,7 @@ btnPromo[0].onclick = prevPromo;
 btnPromo[1].onclick = nextPromo;
 
 var position = 0;
-var blockWidthPromo = 480;
+var blockWidthPromo = screenWidth;
 
 function nextPromo() {
   if (position == -(blockPromo.length - 1) * blockWidthPromo) {
@@ -84,7 +82,7 @@ function prevPromo() {
   } else {
     position = Math.min(position + blockWidthPromo, 0);
   }
-  wrap.style.transform = 'translateX(' + position + 'px)'
+  wrap.style.transform = 'translateX(' + position + 'px)';
 }
 
 //$('.promo__wrap').on("swipeleft", function () {
@@ -93,3 +91,33 @@ function prevPromo() {
 //$('.promo__wrap').on("swiperight", function () {
 //  prevPromo;
 //});
+
+
+
+var btnNew = document.querySelectorAll('.main-content__item--new .main-content__arrow');
+var blockNew = document.querySelector('.main-content__item--new .main-content__row');
+var blockNewItem = document.querySelectorAll('.main-content__item--new .main-content__row .new__item');
+btnNew[0].onclick = prevNew;
+btnNew[1].onclick = nextNew;
+
+var positionNew = 0;
+var blockWidthNew = screenWidth;
+
+function nextNew() {
+  if (positionNew == -(blockNewItem.length - 1) * blockWidthNew) {
+    positionNew = 0;
+  } else {
+    positionNew = Math.max(positionNew - blockWidthNew, -(blockNewItem.length - 1) * blockWidthNew);
+  }
+  blockNew.style.transform = 'translateX(' + positionNew + 'px)'
+};
+
+function prevNew() {
+
+  if (positionNew == 0) {
+    positionNew = -(blockNewItem.length - 1) * blockWidthNew;
+  } else {
+    positionNew = Math.min(position + blockWidthNew, 0);
+  }
+  blockNew.style.transform = 'translateX(' + positionNew + 'px)';
+}
